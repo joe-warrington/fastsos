ad004566c2bb9ccb1cfc1fa471f01d19be4b869c
Joe Warrington <warrington@control.ee.ethz.ch>
2019-07-19 16:07:19 +0200

    Reorder sos.h entries

--- sos.h
+++ sos.h
@@ -17,6 +17,20 @@ unsigned long int a_choose_b(int a, int b);
 
 unsigned long int n_monomials(int n, int d);
 
+unsigned long int get_deg_start(int n, int deg_this_level);
+
+template <class T>
+void print_vec(const string& string_in, const T& v);
+
+string monomial_to_string(const vector<int> v_in);
+
+vector<int> add_vecs(const vector <int> v1, const vector <int> v2);
+
+vector<int> sub_vecs(const vector <int> v1, const vector <int> v2);
+
+template <class T>
+T sum_vec(const vector <T>& v);
+
 void eliminate_unused_dims(int& n, vector<vector<int> >& f_exps, vector<vector<vector<int> > >& g_exps_list,
                            vector<vector<vector<int> > >& h_exps_list, int output_level);
 
@@ -29,23 +43,14 @@ tuple<Model::t, Variable::t, Variable::t, vector<Variable::t>, vector<unsigned l
         PolyInfo& f_info, vector<PolyInfo>& g_infos, vector<PolyInfo>& h_infos,
         int n, int d, unsigned long int s_of_d, string positivity_condition);
 
+int compute_legal_d(PolyInfo f_info, vector<PolyInfo> g_infos, vector<PolyInfo> h_infos, int d_request);
+
 void create_coeff_matches(Model::t& M, vector<double>& f_mono_coeffs, vector<vector<int> >& f_mono_exponents,
                           vector<vector <double> >& g_mono_coeffs, vector<vector <vector <int> > >& g_mono_exponents,
                           vector<vector <double> >& h_mono_coeffs, vector<vector <vector <int> > >& h_mono_exponents,
                           int n, int d, unsigned long int s_of_d, Variable::t& lambda, Variable::t& sigma_0,
                           vector<Variable::t>& sigma_j, vector<unsigned long int>& s_of_d_minus_djs,
                           vector<Variable::t>& tau_j, vector<unsigned long int>& s_of_d_minus_dj2s, int output_level);
-
-int compute_legal_d(PolyInfo f_info, vector<PolyInfo> g_infos, vector<PolyInfo> h_infos, int d_request);
-
-template <class T>
-void print_vec(const string& string_in, const T& v);
-
-vector<int> add_vecs(vector <int> v1, vector <int> v2);
-vector<int> sub_vecs(vector <int> v1, vector <int> v2);
-
-template <class T>
-T sum_vec(const vector <T>& v);
 
 tuple<double, ProblemStatus, SolutionStatus, SolutionStatus> sos_level_d(
         string& f_string, vector<string>& g_strings, vector<string>& h_strings,
